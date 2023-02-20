ce3c99768ac20a9f14fb673990bcdb716ddd53bf
Brian Hoenle <bhoenle@norwex.com>
2023-02-20 14:49:05 -0500

    udemy-rails-course-homework-assignment-one Add functionality to create a new article.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,29 @@
 class ArticlesController < ApplicationController
 
-  def show
-    @article = Article.find(params[:id])
+  def create
+    @article = Article.new(params.require(:article).permit(:title,
+                                                           :description))
+    #render plain: @article.inspect
+    if @article.save
+      flash[:notice] = "Article was successfully saved"
+
+      #redirect_to article_path(@article) or
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   def index
     @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
 end
