986801302485d5e542f8fa6f89c1535f6af4d65c
Brian Hoenle <bhoenle@norwex.com>
2023-02-20 16:14:42 -0500

    udemy-rails-course-homework-assignment-one Add functionality to delete an existing article.

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   root "pages#home"
   get "about", to: "pages#about"
 
-  resources :articles, only: [ :show, :index, :create, :new, :edit, :update ]
+  resources :articles
+
 end
