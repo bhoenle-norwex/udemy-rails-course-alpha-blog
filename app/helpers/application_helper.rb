96db46fac4113619d508430823aa4cb9b3f81896
Brian Hoenle <bhoenle@norwex.com>
2023-02-22 14:12:04 -0500

    udemy-rails-course-homework-assignment-one Adjust links and colors.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Returns the "active" class if the current url is the specified action's page
+  def is_active(action)
+    if params[:action] == "index"
+      params[:controller] == action ? "active" : nil
+    else
+      params[:action] == action ? "active" : nil
+    end
+  end
+
 end
