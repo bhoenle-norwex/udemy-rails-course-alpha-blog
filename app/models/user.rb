dea7b251c285a43ebdd833d57f25b8f37f5ab0a9
Brian Hoenle <bhoenle@norwex.com>
2023-02-23 14:32:50 -0500

    udemy-rails-course Change the state of the user object (email) to be lowercase (downcase)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
   has_many :articles
+  before_save { self.email = email.downcase }
 end
